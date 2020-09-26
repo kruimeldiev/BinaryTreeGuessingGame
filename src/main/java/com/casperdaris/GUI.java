31b98aaa7d81313a460382de76c198e61043c0bd
Casper Daris <casperdaris@msn.com>
2020-09-26 20:21:56 +0200

    JavaFX toegevoegd

---
+++ src/main/java/com/casperdaris/GUI.java
@@ -0,0 +1,11 @@
+package com.casperdaris;
+
+import javafx.scene.layout.GridPane;
+
+public class GUI {
+
+    public GUI(GridPane gp) {
+
+    }
+
+}
--- src/main/java/com/casperdaris/Main.java
+++ src/main/java/com/casperdaris/Main.java
@@ -1,8 +1,25 @@
 package com.casperdaris;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        GridPane root = new GridPane();
+        Scene scene = new Scene(root);
+        new GUI(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
+
+        launch();
 
         BTController controller = new BTController();
 
